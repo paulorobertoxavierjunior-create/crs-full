bd3e24acb62f83f6b80efc59e11870fcd3720aff
paulorobertoxavierjunior-create <paulorobertoxavierjunior@gmail.com>
2026-06-06 16:52:01 -0400

    Criar backend/init-db.sql

---
+++ backend/init-db.sql
@@ -0,0 +1,7 @@
+-- Criar o banco de dados se não existir
+CREATE DATABASE crs_db OWNER crs_user;
+
+-- Conectar ao banco e criar extensões (se necessário)
+\c crs_db
+
+-- Aqui você pode adicionar mais comandos SQL se precisar
