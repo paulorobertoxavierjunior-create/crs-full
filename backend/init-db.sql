0d17d8dc94961d9c9f4a27f3b9f74f148197e850
paulorobertoxavierjunior-create <paulorobertoxavierjunior@gmail.com>
2026-06-06 17:34:21 -0400

    Atualizar o init-db.sql

--- backend/init-db.sql
+++ backend/init-db.sql
@@ -1,7 +1,14 @@
--- Criar o banco de dados se não existir
+-- Criar o usuário se não existir
+CREATE USER crs_user WITH PASSWORD 'crs_password';
+
+-- Criar o banco de dados
 CREATE DATABASE crs_db OWNER crs_user;
 
--- Conectar ao banco e criar extensões (se necessário)
+-- Conectar ao banco e conceder permissões
 \c crs_db
 
--- Aqui você pode adicionar mais comandos SQL se precisar
+-- Conceder todas as permissões ao usuário
+GRANT ALL PRIVILEGES ON DATABASE crs_db TO crs_user;
+GRANT ALL PRIVILEGES ON SCHEMA public TO crs_user;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO crs_user;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO crs_user;
